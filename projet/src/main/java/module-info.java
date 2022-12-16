4d1783da96d6bdf8650b6ac33e9eaa4372f40703
Nzosim <nzosim54@gmail.com>
2022-12-16 08:41:59 +0100

    test

--- projet/src/main/java/module-info.java
+++ projet/src/main/java/module-info.java
@@ -1,12 +1,8 @@
 module com.example.projet {
     requires javafx.controls;
     requires javafx.fxml;
-
-
+            
+                            
     opens com.example.projet to javafx.fxml;
     exports com.example.projet;
-    exports com.example.projet.Modele;
-    opens com.example.projet.Modele to javafx.fxml;
-    exports com.example.projet.Controleur;
-    opens com.example.projet.Controleur to javafx.fxml;
 }
