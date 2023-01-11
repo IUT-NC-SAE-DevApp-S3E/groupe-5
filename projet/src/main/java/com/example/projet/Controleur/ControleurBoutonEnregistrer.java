acffae59f3d68c37abb5ff7ad98c5830114cd4a5
Nzosim <nzosim54@gmail.com>
2023-01-11 11:20:09 +0100

    ajout export vers source plantuml

--- projet/src/main/java/com/example/projet/Controleur/ControleurBoutonEnregistrer.java
+++ projet/src/main/java/com/example/projet/Controleur/ControleurBoutonEnregistrer.java
@@ -2,6 +2,7 @@ package com.example.projet.Controleur;
 
 import com.example.projet.Modele.Modele;
 import com.example.projet.Modele.Sujet;
+import com.example.projet.Utilitaires.Classe;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.FileChooser;
@@ -9,6 +10,7 @@ import javafx.stage.FileChooser;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class ControleurBoutonEnregistrer implements EventHandler<ActionEvent> {
 
@@ -30,6 +32,7 @@ public class ControleurBoutonEnregistrer implements EventHandler<ActionEvent> {
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle("Enregistrer le diagramme");
             fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Diagramme UML", "*.diagramme"));
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Source plantUML", "*.txt"));
             fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Capture d'écran JPG", "*.jpg"));
             fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG PlantUML", "*.png"));
             fileChooser.setInitialFileName("diagramme");
@@ -45,9 +48,36 @@ public class ControleurBoutonEnregistrer implements EventHandler<ActionEvent> {
                 System.out.printf("Serialized data is saved");
             } else if(file.getName().endsWith(".jpg")) {
 
-            } else if(file.getName().endsWith(".png"))
-            {
+            } else if(file.getName().endsWith(".png")) {
 
+            } else if(file.getName().endsWith(".txt")) {
+                ArrayList<Classe> listeClasses = this.sujet.getListeFichiers();
+                String res = "@startuml\n";
+                for(Classe c : listeClasses) {
+                    res += c.toPlantUML() + "\n";
+                }
+                for(Classe c : listeClasses) {
+                    String parent = c.depExtend();
+                    // on ajoute la dépendance uniquement si la classe existe dans la liste des classes
+                    for(Classe c2 : listeClasses) {
+                        if(c2.getNom().equals(parent)) {
+                            res += c.getNom() + " --|> " + parent + "\n";
+                        }
+                    }
+                    ArrayList<String> dependances = c.depImplement();
+                    for(String s : dependances) {
+                        // on ajoute la dépendance uniquement si la classe existe dans la liste des classes
+                        for(Classe c2 : listeClasses) {
+                            if(c2.getNom().equals(s)) {
+                                res += c.getNom() + " ..|> " + s + "\n";
+                            }
+                        }
+                    }
+                }
+
+                res+="@enduml";
+                FileOutputStream fileOut = new FileOutputStream(file.getAbsoluteFile());
+                fileOut.write(res.getBytes());
             }
 
         } catch (Exception e) {
