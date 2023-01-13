3d73eab31087ac9614fc6a196d6ee688065c2a4b
Nzosim <nzosim54@gmail.com>
2023-01-13 10:46:25 +0100

    javadoc fleche

--- projet/src/main/java/com/example/projet/Vue/Fleches/DecorateurFleche.java
+++ projet/src/main/java/com/example/projet/Vue/Fleches/DecorateurFleche.java
@@ -4,12 +4,27 @@ import com.example.projet.Modele.Sujet;
 import com.example.projet.Vue.Observateur;
 import javafx.scene.shape.Line;
 
+/**
+ * Classe DecorateurFleche qui permet de décorer une flèche
+ */
 public class DecorateurFleche extends Line implements Observateur {
+
+    /**
+     * Constructeur de la classe DecorateurFleche
+     * @param coordXDepart les coordonnées en X du départ
+     * @param coordYDepart les coordonnées en Y du départ
+     * @param coordXArrivee les coordonnées en X de l'arrivée
+     * @param coordYArrivee les coordonnées en Y de l'arrivée
+     */
     public DecorateurFleche(int coordXDepart, int coordYDepart, int coordXArrivee, int coordYArrivee)
     {
         super(coordXDepart, coordYDepart, coordXArrivee, coordYArrivee);
     }
 
+    /**
+     * Methode qui permet de mettre a jour l'observateur
+     * @param s le sujet
+     */
     @Override
     public void actualiser(Sujet s) {
 
--- projet/src/main/java/com/example/projet/Vue/Fleches/FinFlecheVide.java
+++ projet/src/main/java/com/example/projet/Vue/Fleches/FinFlecheVide.java
@@ -2,8 +2,20 @@ package com.example.projet.Vue.Fleches;
 
 import javafx.scene.paint.Color;
 
+/**
+ * Classe FinFlecheVide qui permet de décorer la fin de la flèche
+ */
 public class FinFlecheVide extends DecorateurFinFleche
 {
+
+    /**
+     * Constructeur de la classe FinFlecheVide
+     * @param image l'image
+     * @param coordXDepart les coordonnées en X du départ
+     * @param coordYDepart les coordonnées en Y du départ
+     * @param coordXArrivee les coordonnées en X de l'arrivée
+     * @param coordYArrivee les coordonnées en Y de l'arrivée
+     */
     public FinFlecheVide(String image, int coordXDepart, int coordYDepart, int coordXArrivee, int coordYArrivee) {
         super(image, coordXDepart, coordYDepart, coordXArrivee, coordYArrivee);
     }
