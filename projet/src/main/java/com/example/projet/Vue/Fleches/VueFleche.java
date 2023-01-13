3a25c4e7ed8f37c0f3d9bd8ebdd5ab890f844954
Bozeyy <spereaux2@gmail.com>
2023-01-13 10:10:29 +0100

    correction problème

--- projet/src/main/java/com/example/projet/Vue/Fleches/VueFleche.java
+++ projet/src/main/java/com/example/projet/Vue/Fleches/VueFleche.java
@@ -16,17 +16,17 @@ public class VueFleche extends Group implements Observateur
         if(type == 1)
         {
             fleche = new VueFlechePleine(xd, yd, xa, ya);
-            finFleche = new FinFlecheVide("ArrowVide.png",xd, yd, xa, ya);
+            finFleche = new FinFlecheVide("ArrowNonPleine.png",xd, yd, xa, ya);
         }
         else if (type == 2)
         {
             fleche = new VueFlechePointille(xd, yd, xa, ya);
-            finFleche = new FinFlecheVide("ArrowVide.png", xd, yd, xa, ya);
+            finFleche = new FinFlecheVide("ArrowNonPleine.png", xd, yd, xa, ya);
         }
         else if (type == 3)
         {
             fleche = new VueFlechePleine(xd, yd, xa, ya);
-            finFleche = new FinFlecheRempli("ArrowFill.png",xd, yd, xa, ya);
+            finFleche = new FinFlecheRempli("ArrowPleine.png",xd, yd, xa, ya);
         }
         this.getChildren().addAll(fleche, finFleche);
     }
